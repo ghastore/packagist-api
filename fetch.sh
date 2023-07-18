a15fc95fef800fd9ec7b528bb49c437771ee463c
KitsuneSolar <kitsune.solar@gmail.com>
2023-07-18 23:18:57 +0000

    2023-07-18 23:18:57

--- fetch.sh
+++ fetch.sh
@@ -1,4 +1,14 @@
 #!/bin/bash -e
+#
+# Get Packagist API responses and save them to repository.
+#
+# @package    Bash
+# @author     Kitsune Solar <mail@kitsune.solar>
+# @copyright  2023 iHub TO
+# @license    MIT
+# @version    0.0.1
+# @link       https://github.com/ghastore
+# -------------------------------------------------------------------------------------------------------------------- #
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # CONFIGURATION.
